11ede3be47a3415ef3a78809a8520fcca349e119
정서연 <jjungseoyeon@naver.com>
2022-01-14 22:13:17 +0900

    second lesson

--- DaelimUniv/src/main/java/com/daelim/univ/member/controller/MemberController.java
+++ DaelimUniv/src/main/java/com/daelim/univ/member/controller/MemberController.java
@@ -10,8 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.daelim.univ.member.domain.Member;
@@ -21,6 +26,43 @@ public class MemberController {
 
 	private static final Logger logger = 
 			LoggerFactory.getLogger(MemberController.class);
+	
+	@GetMapping("/member/read")
+	public String readForm(Model model) {
+		logger.info("MemberController, readForm.");
+		
+		List<String> resultList1 = new ArrayList<String>();
+		
+		resultList1.add("한화");
+		resultList1.add("한화시스템");
+		resultList1.add("한화솔루션");
+		resultList1.add("한화에어로스페이스");
+		resultList1.add("한화호텔앤리조트");
+		resultList1.add("한화건설");
+		
+		model.addAttribute("company", resultList1);
+		
+		return "member/read";
+	}
+	
+	@GetMapping("/member/registerForm")
+	public String registerForm() {
+		logger.info("registerForm.");
+		
+		return "member/registerForm";
+	}
+	
+	@PostMapping("/member/register")
+	public String getRegister(Member member) {
+		logger.info("MemberController, getRegister.");
+		
+		if (member != null) {
+			System.out.println("member : " + member.toString());
+		}
+		
+		return "member/success";
+	}
+	
 	
 	@ResponseBody
 	@RequestMapping(value = "/member/home0301", method = RequestMethod.GET)
@@ -80,7 +122,57 @@ public class MemberController {
 		
 		String resultStr = String.valueOf("Easy day is yesterday.");
 		
-		return new ResponseEntity<String>(resultStr, HttpStatus.OK);
+		return new ResponseEntity<String>(resultStr, HttpStatus.OK);	
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/member/home0504", method = RequestMethod.GET)
+	public ResponseEntity<String> home0504() {
+		logger.info("home0504.");
 		
+		return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/member/home0505", method = RequestMethod.GET)
+	public ResponseEntity<Member> home0505() {
+		logger.info("home0505.");
+		
+		Member member = new Member();
+		
+		return new ResponseEntity<Member>(member, HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/member/home0506", method = RequestMethod.GET)
+	public ResponseEntity<List<Member>> home0506() {
+		logger.info("home0506.");
+		
+		List<Member> resultList = new ArrayList<Member>();
+		
+		Member member = new Member();
+		resultList.add(member);
+		
+		member = new Member();
+		resultList.add(member);
+		
+		member = new Member();
+		resultList.add(member);
+		
+		return new ResponseEntity<List<Member>>(resultList, HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/member/home0507", method = RequestMethod.GET)
+	public ResponseEntity<HashMap<String, Object>> home0507() {
+		logger.info("home0507.");
+		
+		HashMap<String, Object> resultMap = new HashMap<String,Object>();
+		
+		resultMap.put("이름", "홍길동");
+		resultMap.put("재산", "1000000000000");
+		resultMap.put("대출", "100000");
+		
+		return new ResponseEntity<HashMap<String,Object>>(resultMap, HttpStatus.OK);
 	}
 }
--- DaelimUniv/src/main/java/com/daelim/univ/member/domain/Member.java
+++ DaelimUniv/src/main/java/com/daelim/univ/member/domain/Member.java
@@ -2,19 +2,31 @@ package com.daelim.univ.member.domain;
 
 public class Member {
 
-	private String userName = "jungseoyeon";
-	private String password = "1234";
+	private String userId;
+	private String password;
+	private String job;
 	
-	public String getUserName() {
-		return userName;
+	public String getUserId() {
+		return userId;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	@Override
+	public String toString() {
+		return "Member [userId=" + userId + ", password=" + password + ", job=" + job + "]";
 	}
 }
