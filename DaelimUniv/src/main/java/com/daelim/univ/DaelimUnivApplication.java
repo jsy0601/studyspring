3588154eb377ab061ac0edd2a0c8a064d1f611f5
정서연 <jjungseoyeon@naver.com>
2022-01-21 21:34:24 +0900

    4th lesson

--- DaelimUniv/src/main/java/com/daelim/univ/DaelimUnivApplication.java
+++ DaelimUniv/src/main/java/com/daelim/univ/DaelimUnivApplication.java
@@ -1,8 +1,10 @@
 package com.daelim.univ;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackageClasses = DaelimUnivApplication.class)
 @SpringBootApplication
 public class DaelimUnivApplication {
 
